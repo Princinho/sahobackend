44d1c3d55ad780b3575e02c4a6c65061637ab9dc
Princinho <princegnakou@gmail.com>
2026-02-25 16:33:28 +0000

    allow all origins for testing

--- main.go
+++ main.go
@@ -29,7 +29,7 @@ func main() {
 
 	r := gin.Default()
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:8081"},
+		AllowOrigins:     []string{"*"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
